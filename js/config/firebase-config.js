9bd44267043f5d4a3710095b8f9c9fded59ea126
Henriqueprofessor <henriqueadas2022@gmail.com>
2026-06-19 20:09:50 -0300

    Update firebase-config.js

--- js/config/firebase-config.js
+++ js/config/firebase-config.js
@@ -1,9 +1,9 @@
 window.firebaseConfig = {
-  apiKey: "AIzaSyAA_5rBa-eQ46l1y3Kg8B-VBzO-o2HVhEE",
+  apiKey: "AIzaSyAA_5rBa-eQ4611y3Kg8B-VBzO-o2HVhEE",
   authDomain: "final-copa-tabuada.firebaseapp.com",
   databaseURL: "https://final-copa-tabuada-default-rtdb.firebaseio.com",
   projectId: "final-copa-tabuada",
   storageBucket: "final-copa-tabuada.firebasestorage.app",
-  messagingSenderId: "1000000014111",
-  appId: "1:488285824115:web:50e94f68253473aee91b06"
+  messagingSenderId: "488825824115",
+  appId: "1:488825824115:web:50e94f68253473aee91b06"
 };
