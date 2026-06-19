b516aea0a4f210246ced2cb5014e476cb79f40fa
Henriqueprofessor <henriqueadas2022@gmail.com>
2026-06-19 05:40:05 -0300

    Create firebase-config.js

---
+++ js/config/firebase-config.js
@@ -0,0 +1,10 @@
+// js/config/firebase-config.js
+window.firebaseConfig = {
+  apiKey: "AIzaSyAA_5rBa-eQ4611y3Kg8B-VBz0-o2HVhEE",
+  authDomain: "final-copa-tabuada.firebaseapp.com",
+  databaseURL: "https://final-copa-tabuada-default-rtdb.firebaseio.com",
+  projectId: "final-copa-tabuada",
+  storageBucket: "final-copa-tabuada.firebasestorage.app",
+  messagingSenderId: "488825824115",
+  appId: "1:488825824115:web:50e94f68253473aee91b06"
+};
