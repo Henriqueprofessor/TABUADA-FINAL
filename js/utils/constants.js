d5e714982fef4c31b3e2486b80f3d6edf2b130f8
Henriqueprofessor <henriqueadas2022@gmail.com>
2026-06-19 05:12:57 -0300

    Create constants.js

---
+++ js/utils/constants.js
@@ -0,0 +1,19 @@
+window.TOTAL_FASES = 5;
+
+window.VAGAS_POR_FASE = {
+  1: 30,
+  2: 20,
+  3: 10,
+  4: 5,
+  5: 5
+};
+
+window.MODALIDADE_CONFIG = {
+  "2-5": { min: 2, max: 5, nome: "Tabuada 2️⃣➡️5️⃣" },
+  "6-9": { min: 6, max: 9, nome: "Tabuada 6️⃣➡️9️⃣" },
+  "0-10": { min: 0, max: 10, nome: "Tabuada 0️⃣➡️🔟 (Completa)" }
+};
+
+window.TURMAS_PADRAO = ["901", "1001", "1002", "1003", "1004", "2001", "2002", "2003", "3001", "3002"];
+
+window.TOTAL_PERGUNTAS = 20;
