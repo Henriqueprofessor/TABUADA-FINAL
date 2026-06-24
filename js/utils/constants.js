b0d934472e15f205fd0a2a469720580123de0c5c
Henriqueprofessor <henriqueadas2022@gmail.com>
2026-06-24 11:44:38 -0300

    Update constants.js

--- js/utils/constants.js
+++ js/utils/constants.js
@@ -1,6 +1,7 @@
-window.TOTAL_FASES = 5;
+// js/utils/constants.js
+export const TOTAL_FASES = 5;
 
-window.VAGAS_POR_FASE = {
+export const VAGAS_POR_FASE = {
   1: 30,
   2: 20,
   3: 10,
@@ -8,12 +9,12 @@ window.VAGAS_POR_FASE = {
   5: 5
 };
 
-window.MODALIDADE_CONFIG = {
+export const MODALIDADE_CONFIG = {
   "2-5": { min: 2, max: 5, nome: "Tabuada 2️⃣➡️5️⃣" },
   "6-9": { min: 6, max: 9, nome: "Tabuada 6️⃣➡️9️⃣" },
   "0-10": { min: 0, max: 10, nome: "Tabuada 0️⃣➡️🔟 (Completa)" }
 };
 
-window.TURMAS_PADRAO = ["901", "1001", "1002", "1003", "1004", "2001", "2002", "2003", "3001", "3002"];
+export const TURMAS_PADRAO = ["901", "1001", "1002", "1003", "1004", "2001", "2002", "2003", "3001", "3002"];
 
-window.TOTAL_PERGUNTAS = 20;
+export const TOTAL_PERGUNTAS = 20;
