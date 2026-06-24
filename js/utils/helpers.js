6de8655b39e6015597830c2d3d8b12f56478dc0f
Henriqueprofessor <henriqueadas2022@gmail.com>
2026-06-24 11:45:02 -0300

    Update helpers.js

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -1,21 +1,24 @@
-window.toast = function(message) {
-  var t = document.getElementById('toast');
+// js/utils/helpers.js
+
+// Toast
+export function toast(message) {
+  const t = document.getElementById('toast');
   if (t) {
     t.innerText = message;
     t.classList.remove('hidden');
-    setTimeout(function() { t.classList.add('hidden'); }, 3000);
+    setTimeout(() => t.classList.add('hidden'), 3000);
   }
-};
+}
 
-window.updateLastSyncTime = function() {
-  var span = document.getElementById('last-sync-time');
+export function updateLastSyncTime() {
+  const span = document.getElementById('last-sync-time');
   if (span) {
     span.innerText = new Date().toLocaleString('pt-BR');
   }
-  window.toast('Sincronizado!');
-};
+  toast('Sincronizado!');
+}
 
-window.escapeHtml = function(str) {
+export function escapeHtml(str) {
   if (!str) return '';
   return str.replace(/[&<>]/g, function(m) {
     if (m === '&') return '&amp;';
@@ -23,31 +26,27 @@ window.escapeHtml = function(str) {
     if (m === '>') return '&gt;';
     return m;
   }).replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, function(c) { return c; });
-};
+}
 
-window.debounce = function(fn, delay) {
-  var timer = null;
-  return function() {
-    var args = arguments;
-    var context = this;
+export function debounce(fn, delay) {
+  let timer = null;
+  return function(...args) {
     if (timer) clearTimeout(timer);
-    timer = setTimeout(function() {
-      fn.apply(context, args);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
       timer = null;
     }, delay);
   };
-};
+}
 
-window.shuffleArray = function(arr) {
-  for (var i = arr.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+export function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
-};
+}
 
-window.randInt = function(min, max) {
+export function randInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
