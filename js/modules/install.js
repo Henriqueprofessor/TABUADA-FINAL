3a5eecaf45b7740d4a0aa48f277a7053f24dd228
Henriqueprofessor <henriqueadas2022@gmail.com>
2026-07-03 20:28:14 -0300

    Create install.js

---
+++ js/modules/install.js
@@ -0,0 +1,6 @@
+export function abrirInstalacao() {
+  // Abre a página de tutorial de instalação
+  window.open('tutorial-instalacao.html', '_blank');
+  // Ou, se preferir um modal:
+  // exibirToast('📱 Siga as instruções para adicionar à tela inicial.');
+}
