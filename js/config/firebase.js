2377e55499001eb41b0d4c2b596437fa28dffc77
Henriqueprofessor <henriqueadas2022@gmail.com>
2026-07-03 21:08:41 -0300

    Update firebase.js

--- js/config/firebase.js
+++ js/config/firebase.js
@@ -1,6 +1,19 @@
 // js/config/firebase.js
+// Inicializa o Firebase com as configurações
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkoL3Zn-YkGUy6RAWZYVhWtIbcfL8h-J8",
+  authDomain: "final-copa-tabuada.firebaseapp.com",
+  databaseURL: "https://final-copa-tabuada-default-rtdb.firebaseio.com",
+  projectId: "final-copa-tabuada",
+  storageBucket: "final-copa-tabuada.firebasestorage.app",
+  messagingSenderId: "488825824115",
+  appId: "1:488825824115:web:50e94f68253473aee91b06"
+};
+
 // O Firebase já foi carregado pelas tags script no HTML,
 // então a variável global 'firebase' está disponível.
+window.firebase.initializeApp(firebaseConfig);
 
 const db = window.firebase.database();
 const auth = window.firebase.auth();
