a88f78369a6f48117a381ae29f9a40183b70e606
Henriqueprofessor <henriqueadas2022@gmail.com>
2026-07-09 23:27:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 // SERVICE WORKER – Copa Tabuada CEIB 2026
 // ============================================================
 
-const CACHE_STATIC_NAME = 'copa-tabuada-v1.3';
-const CACHE_DYNAMIC_NAME = 'copa-tabuada-dynamic-v1.3';
+const CACHE_STATIC_NAME = 'copa-tabuada-v1.6';
+const CACHE_DYNAMIC_NAME = 'copa-tabuada-dynamic-v1.6';
 
 // ===== CAMINHOS RELATIVOS (SEM BARRA INICIAL) =====
 const STATIC_ASSETS = [
